b94be3b848e7786fb0380990c267ebcaf030b7bb
Katanyoo_2003 <tatakatanyoo2546@gmail.com>
2024-09-08 15:23:49 +0700

    lab01 file

---
+++ lab01.py
@@ -0,0 +1,37 @@
+def mul_a_b(a,b):
+    ...
+    print(a*b)
+
+mul_a_b(3, 4)
+
+def circlecal (r):
+    return 3.14*r*r
+
+print(circlecal(5))
+
+for i in range (5) :
+    print(i+1)
+    print (circlecal(i+1))
+
+area = []
+
+for i in range (1, circle_number+1):
+    area = cal_circle_area
+
+
+
+areas.append(10)
+print(sum(areas))
+
+# Challenge 2 !
+# Creating a function to calculate the area of circle.
+# Users will specify the number of circle,
+# The program will calculate the area of each circle with radius -1 
+# for example
+# input : 5
+# process: [The area of circle of radius 1-5 will be calculated].
+# output: [Print all area on the screen].
+
+grades = {'Mark' :'A',
+          'Jib': 'B'}
+print(grades ['Mark']) # A 
